4cda2d6176dafad77f64412aef05af8924fef790
PelgyeDorji <12240057.gcit@rub.edu.bt>
2025-10-21 00:58:20 +0600

    Added 2 features of backend using java and created folders to store.

---
+++ src/main/java/com/kanban/Stage.java
@@ -0,0 +1,65 @@
+package com.kanban;
+
+public class Stage{
+    private String name;
+    private Task[] tasks;
+    private int count;
+
+    public Stage(String stageName){
+        name = stageName;
+        tasks = new Task[100]; //fixed max for simplicity
+        count=0;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public boolean addTask(Task t){
+        if(count < tasks.length && t != null){
+            tasks[count] = t;
+            count++;
+            return true;
+        }
+        return false;
+    }
+
+
+    public boolean removeTaskById(int id){
+        for(int i = 0; i<count; i++){
+            if(tasks[i].getId() == id){
+                for(int j = i; j < count-1; j++){
+                    tasks[j] = tasks[j+1];
+                }
+                count--;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Task getTaskById(int id){
+        for(int i = 0; i < count; i++){
+            if(tasks[i].getId() == id){
+                return tasks[i];
+            }
+        }
+        return null;
+    }
+
+
+    public void showAllTasks() {
+        System.out.println("=== "+ name+ " ===");
+        if(count == 0){
+            System.out.println("No tasks here yet.\n);
+            return;
+        }
+        for(int i=0; i<count; i++){
+            tasks[i].displayTask();
+        }
+    }
+
+    public int getTaskCount(){
+        return coun;
+    }
+}
---
+++ src/main/java/com/kanban/Task.java
@@ -0,0 +1,85 @@
+package com.kanban;
+
+public class Task{
+    private staticint nextId = 1; // simple auto-ID generator
+    private int id;
+    private String Title;
+    private String description;
+    private String priority; // High, Medium, Low
+    private String assignee;
+    private String status; // TO-DO, PROGRESS, DONE
+
+    public Task(){
+        id = nextId++;
+        Title = "";
+        description = "";
+        priority = "Low";
+        assignee = "Unassigned";
+        status = "TO-DO";
+    }
+
+    public Task(String tit, String desc, String assign){
+        id = nextId++;
+        if(tit == null || desc.trin().equals("")){
+            Title = "Untitled Task";
+        } else {
+            Title = tit;
+        }
+
+        if(desc == null || desc.trin().equals("")){
+            description = "Enter description";
+        } else{
+            description = desc;
+        }
+
+        if(assign == null || assign.trim().equals("")){
+            assignee = "Unassigned";
+        } else{
+            assignee = assign;
+        }
+
+        priority = "Low";
+        status = "TO-DO";
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public String getTitle(){
+        return Title;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public String getPriority(){
+        return priority;
+    }
+
+    public String getAssignee(){
+        return assignee;
+    }
+
+    public String getStatus(){
+        return status;
+    }
+
+    public void setPriority(String newPriority){
+        priority = newPriority;
+    }
+
+    public void setStatus(String newStatus){
+        status = newStatus;
+    }
+
+    public void displayTask() {
+        System.out.println("Task ID: "+ id);
+        System.out.println("Title: "+ Title);
+        System.out.println("Description: "+ description);
+        System.out.println("Priority: "+ priority);
+        System.out.println("Assignee: "+ assignee);
+        System.out.println("Status: "+ status);
+    }
+}
